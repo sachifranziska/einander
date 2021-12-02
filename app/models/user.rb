b478c44926d73f6182e4786a44571177d363a704
sachifranziska <s.shibata871@gmail.com>
2021-12-02 15:36:13 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,18 @@ class User < ApplicationRecord
   belongs_to :experience
   belongs_to :gender
   belongs_to :stay_length
+
+  with_options presence: true, numericality: { other_than: 1, message: "can't be blank"} do
+    validates :country_id
+    validates :experience_id
+    validates :gender_id
+    validates :stay_length_id
+  end
   
-  validates :country_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :experience_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :gender_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :stay_length_id, numericality: { other_than: 1, message: "can't be blank"}
-  
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :birth_date
+  end 
 end
