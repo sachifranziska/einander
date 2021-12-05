cc6945a1c3dc83d2758c3517528a60902e3b2cb0
sachifranziska <s.shibata871@gmail.com>
2021-12-05 12:14:35 +0900

    相談詳細画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "questions#index"
-  resources :questions, only: [:index, :new, :create]
+  resources :questions, only: [:index, :new, :create, :show]
 end
