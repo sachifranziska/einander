935ccbf920de7de30a4aa409e3226e9dec948c3f
sachifranziska <s.shibata871@gmail.com>
2021-12-06 20:08:50 +0900

    バリデーション、ストロングパラメーター

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,10 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :category_id, :content, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,14 @@
 class Blog < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  with_options presence: true do
+    validates :title
+    validates :content
+  end 
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
 end
