3b7c90b4260adb4a5bcd958969ed1798cb55da94
sachifranziska <s.shibata871@gmail.com>
2021-12-06 20:20:23 +0900

    createアクション定義

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,6 +3,15 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
 
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def blog_params
