70bae484245e81877fd5a5a801b2fee758f4b691
sachifranziska <s.shibata871@gmail.com>
2021-12-08 13:20:22 +0900

    編集機能(edit,updateアクション)

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -19,6 +19,19 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id])
   end
 
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blog_path(@blog.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def blog_params
