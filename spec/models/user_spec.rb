47d1594e0aeecd330f079a7b62ea146d76596506
sachifranziska <s.shibata871@gmail.com>
2022-01-14 11:28:59 +0900

    Userモデルのテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,44 +20,85 @@ RSpec.describe User, type: :model do
       end
 
       it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
       end
 
       it '重複したemailが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
 
       it 'emailが@を含まない場合登録できない' do
+        @user.email = 'samplecom'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
 
       it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
       end
 
       it 'passwordが5文字以下では登録できない' do
+        @user.password = '123ab'
+        @user.password_confirmation = '123ab'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
 
       it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = '123abc'
+        @user.password_confirmation = '123abd'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
       it 'last_nameが空では登録できない' do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
 
       it 'first_nameが空では登録できない' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
       end
 
       it 'birth_dateが空では登録できない' do
+        @user.birth_date = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birth date can't be blank")
       end
 
       it 'country_idが選択されていないと登録できない' do
+        @user.country_id = 1
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Country can't be blank")
       end
 
       it 'experience_idが選択されていないと登録できない' do
+        @user.experience_id = 1
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Experience can't be blank")
       end
 
       it 'gender_idが選択されていないと登録できない' do
+        @user.gender_id = 1
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Gender can't be blank")
       end
 
       it 'stay_length_idが選択されていないと登録できない' do
+        @user.stay_length_id = 1
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Stay length can't be blank")
       end
-
     end
   end
 end
